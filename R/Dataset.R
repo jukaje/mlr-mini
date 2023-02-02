ba7800e74957d6ed2001308a61dff7b1d523c998
Hagen Lamprecht <H.Lamprecht@campus.lmu.de>
2023-02-02 19:46:29 +0100

    print Dataset

--- R/Dataset.R
+++ R/Dataset.R
@@ -1,12 +1,19 @@
 Dataset <- function(data, target, type = "regression", name = deparse(substitute(data), 20)[[1]]){
   
-  class(data) <- c("Dataset", class(data))
-  class(target) <- c("Dataset", class(target))
+  #class(data) <- c("Dataset", "Regression")
+  #class(target) <- c("Dataset", class(target))
   
   structure(list(data = data, target = target, type = type, name = name),
-            class = "Dataset")
+            class = c("DatasetRegression", "Dataset"))
 }
 
+print.Dataset <- function(object) {
+  cat(c("Dataset ", '"',object$name, '"', " predicting ", '"',object$target, '"', ":", "\n\n"), sep = "")
+  print(data.frame(object$data))
+}
 
-Dataset(cars, target = "dist")
+t <- Dataset(cars, target = "dist")
+unclass(t)
+
+
 class(Dataset(cars, target = "dist", type = "dd"))
