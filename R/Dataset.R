990850da6d16068f3c43d580537ed9c9f19e5398
Hagen Lamprecht <H.Lamprecht@campus.lmu.de>
2023-02-03 15:14:26 +0100

    metainfo function

--- R/Dataset.R
+++ R/Dataset.R
@@ -12,8 +12,50 @@ print.Dataset <- function(object) {
   print(data.frame(object$data))
 }
 
+'[.Dataset' <- function(obj, i, j = names(obj$data)) {
+  #print(names(obj$data))
+  if (is.character(j) & !(obj$target %in% j)) {
+    cat(c("Error: Cannot remove target column ", 
+         '"', obj$target, '"', "\n"), sep = "")
+  } else if (is.numeric(j) & !(obj$target %in% names(obj$data)[j])) {
+    cat(c("Error: Cannot remove target column ", 
+          '"', obj$target, '"', "\n"), sep = "")
+  } else {
+    data.frame(unclass(obj$data))[i,j]
+  }
+  
+}
+
+
+
+metainfo <- function(data) {
+  features <- names(data$data)[names(data$data) != data$target]
+  targets <- data$target
+  nrow <- nrow(data$data)
+  type <- data$type
+  missings <- any(is.na(data$data))
+  #print(class(data$data[,features]))
+  info <- list(feature = c(lapply(features, function(x) { c(x, class(data$data[,x])) })), 
+               target = c(targets, class(data$data[,targets])),
+               nrow = nrow, type = type, missings = missings)
+  class(info) <- "DatasetInfo"
+  return(info)
+}
+
+
+
 t <- Dataset(cars, target = "dist")
+t[1,1]
+#dataframe
+names(t$data) 
+
 unclass(t)
 
 
 class(Dataset(cars, target = "dist", type = "dd"))
+
+
+cars_info <- metainfo(t)
+print(cars_info)
+
+
